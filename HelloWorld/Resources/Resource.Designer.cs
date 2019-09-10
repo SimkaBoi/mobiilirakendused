9ba9afe439dede9d7b5da0694a925a7239db8307
siimr <simkaviiner@gmail.com>
2019-09-10 09:50:28 +0300

    Add calculator logic

--- HelloWorld/Resources/Resource.Designer.cs
+++ HelloWorld/Resources/Resource.Designer.cs
@@ -2396,8 +2396,8 @@ namespace HelloWorld
 			// aapt resource value: 0x7f08005c
 			public const int action_mode_close_button = 2131230812;
 			
-			// aapt resource value: 0x7f0800b3
-			public const int action_settings = 2131230899;
+			// aapt resource value: 0x7f0800b9
+			public const int action_settings = 2131230905;
 			
 			// aapt resource value: 0x7f08009d
 			public const int action_text = 2131230877;
@@ -2420,6 +2420,12 @@ namespace HelloWorld
 			// aapt resource value: 0x7f080038
 			public const int always = 2131230776;
 			
+			// aapt resource value: 0x7f0800ae
+			public const int arv1 = 2131230894;
+			
+			// aapt resource value: 0x7f0800af
+			public const int arv2 = 2131230895;
+			
 			// aapt resource value: 0x7f080056
 			public const int async = 2131230806;
 			
@@ -2434,9 +2440,6 @@ namespace HelloWorld
 			
 			// aapt resource value: 0x7f08003d
 			public const int bottom = 2131230781;
-			
-			// aapt resource value: 0x7f0800af
-			public const int button1 = 2131230895;
 			
 			// aapt resource value: 0x7f080063
 			public const int buttonPanel = 2131230819;
@@ -2579,11 +2582,23 @@ namespace HelloWorld
 			// aapt resource value: 0x7f080000
 			public const int item_touch_helper_previous_elevation = 2131230720;
 			
+			// aapt resource value: 0x7f0800b4
+			public const int jaga = 2131230900;
+			
+			// aapt resource value: 0x7f0800b3
+			public const int korruta = 2131230899;
+			
+			// aapt resource value: 0x7f0800b2
+			public const int lahuta = 2131230898;
+			
 			// aapt resource value: 0x7f08008e
 			public const int largeLabel = 2131230862;
 			
 			// aapt resource value: 0x7f080049
 			public const int left = 2131230793;
+			
+			// aapt resource value: 0x7f0800b1
+			public const int liida = 2131230897;
 			
 			// aapt resource value: 0x7f080017
 			public const int line1 = 2131230743;
@@ -2591,20 +2606,23 @@ namespace HelloWorld
 			// aapt resource value: 0x7f080018
 			public const int line3 = 2131230744;
 			
+			// aapt resource value: 0x7f0800b0
+			public const int linearLayout1 = 2131230896;
+			
 			// aapt resource value: 0x7f08001d
 			public const int listMode = 2131230749;
 			
 			// aapt resource value: 0x7f080061
 			public const int list_item = 2131230817;
 			
-			// aapt resource value: 0x7f0800b2
-			public const int masked = 2131230898;
+			// aapt resource value: 0x7f0800b8
+			public const int masked = 2131230904;
 			
 			// aapt resource value: 0x7f0800a1
 			public const int media_actions = 2131230881;
 			
-			// aapt resource value: 0x7f0800b0
-			public const int message = 2131230896;
+			// aapt resource value: 0x7f0800b6
+			public const int message = 2131230902;
 			
 			// aapt resource value: 0x7f080031
 			public const int middle = 2131230769;
@@ -2795,9 +2813,6 @@ namespace HelloWorld
 			// aapt resource value: 0x7f080069
 			public const int textSpacerNoTitle = 2131230825;
 			
-			// aapt resource value: 0x7f0800ae
-			public const int textView1 = 2131230894;
-			
 			// aapt resource value: 0x7f08009a
 			public const int text_input_password_toggle = 2131230874;
 			
@@ -2855,11 +2870,14 @@ namespace HelloWorld
 			// aapt resource value: 0x7f080026
 			public const int useLogo = 2131230758;
 			
+			// aapt resource value: 0x7f0800b5
+			public const int vastus = 2131230901;
+			
 			// aapt resource value: 0x7f080016
 			public const int view_offset_helper = 2131230742;
 			
-			// aapt resource value: 0x7f0800b1
-			public const int visible = 2131230897;
+			// aapt resource value: 0x7f0800b7
+			public const int visible = 2131230903;
 			
 			// aapt resource value: 0x7f08003c
 			public const int withText = 2131230780;
--- HelloWorld/SecondActivity.cs
+++ HelloWorld/SecondActivity.cs
@@ -15,11 +15,69 @@ namespace HelloWorld
     [Activity(Label = "SecondActivity")]
     public class SecondActivity : Activity
     {
+        TextView _vastus;
+        Button _liida;
+        Button _lahuta;
+        Button _korruta;
+        Button _jaga;
+        EditText _textField1;
+        EditText _textField2;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.second_layout);
             // Create your application here
+            _textField1 = FindViewById<EditText>(Resource.Id.arv1);
+            _textField2 = FindViewById<EditText>(Resource.Id.arv2);
+            
+            int arv1 = Convert.ToInt32(_textField1.Text);
+            int arv2 = Convert.ToInt32(_textField2.Text);
+            var x = "";
+            _liida.Click += delegate
+            {
+                x = "+";
+                _vastus.Text = Convert.ToString(Arvuta(arv1, arv2, x));
+            };
+            _lahuta.Click += delegate
+            {
+                x = "-";
+                _vastus.Text = Convert.ToString(Arvuta(arv1, arv2, x));
+            };
+            _korruta.Click += delegate
+            {
+                x = "*";
+                _vastus.Text = Convert.ToString(Arvuta(arv1, arv2, x));
+            };
+            _jaga.Click += delegate
+            {
+                x = "/";
+                _vastus.Text = Convert.ToString(Arvuta(arv1, arv2, x));
+            };
+        }
+
+        public int Arvuta(int arv1, int arv2, string x)
+        {
+            if(x == "+")
+            {
+                return arv1 + arv2;
+            }
+            else if(x == "-")
+            {
+                return arv1 - arv2;
+            }
+            else if(x == "*")
+            {
+                return arv1 * arv2;
+            }
+            else if(x == "/")
+            {
+                return arv1 / arv2;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
