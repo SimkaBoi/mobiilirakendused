f321547fded01a199d3ba83cd7eeaf26e41d875e
SimkaBoi <simkaviiner@gmail.com>
2020-02-04 09:34:28 +0200

    edit firstforms design

--- FirstForms/FirstForms/FirstForms/App.xaml.cs
+++ FirstForms/FirstForms/FirstForms/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FirstForms.Data;
+using System;
 using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -8,14 +9,22 @@ namespace FirstForms
 {
     public partial class App : Application
     {
-        public static string FolderPath { get; private set; }
+        static NoteDatabase database;
 
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
-
-            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
-
             MainPage = new NavigationPage(new NotesPage());
         }
 
--- FirstForms/FirstForms/FirstForms/NotesPage.xaml.cs
+++ FirstForms/FirstForms/FirstForms/NotesPage.xaml.cs
@@ -19,25 +19,10 @@ namespace FirstForms
             InitializeComponent();
         }
 
-        protected override void OnAppearing()
+        protected override async void OnAppearing()
         {
             base.OnAppearing();
-
-            var notes = new List<Note>();
-            var files = Directory.EnumerateFiles(App.FolderPath, "*.notes.txt");
-            foreach(var filename in files)
-            {
-                notes.Add(new Note
-                {
-                    Filename = filename,
-                    Text = File.ReadAllText(filename),
-                    Date = File.GetCreationTime(filename)
-                });
-            }
-
-            notesListView.ItemsSource = notes
-                .OrderBy(d => d.Date)
-                .ToList();
+            notesListView.ItemsSource = await App.Database.GetNotesAsync();
         }
 
         async void OnNoteAddedClicked(object sender, EventArgs e)
