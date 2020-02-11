bb1c5f21f5c59983361d10186e04778561faba7e
SimkaBoi <simkaviiner@gmail.com>
2020-02-11 09:51:50 +0200

    add unit tests and design

---
+++ MvvmTutorial/MvvmTutorial.Tests/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,20 @@
+﻿using MvvmTutorial.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MvvmTutorial.Tests.ViewModels
+{
+    public class MainViewModelTests
+    {
+        [Fact]
+        public void Recalculate_SubTotal100_Total120()
+        {
+            var mainViewModel = new MainViewModel();
+            mainViewModel.SubTotal = 100;
+            mainViewModel.Generosity = 0.2;
+            Assert.Equal(120, mainViewModel.Total);
+        }
+    }
+}
