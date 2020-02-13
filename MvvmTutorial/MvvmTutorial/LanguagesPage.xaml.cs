843ded80efe846497aa9314991bc38ef94200376
SimkaBoi <simkaviiner@gmail.com>
2020-02-13 09:53:36 +0200

    add tabbedpage

---
+++ MvvmTutorial/MvvmTutorial/LanguagesPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using MvvmTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MvvmTutorial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LanguagesPage : ContentPage
+    {
+        public LanguagesPage()
+        {
+            InitializeComponent();
+            LanguageList.ItemsSource = new List<LanguageModel>()
+            {    
+            };
+        }
+    }
+}
---
+++ MvvmTutorial/MvvmTutorial/Models/LanguageModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvmTutorial.Models
+{
+    class LanguageModel
+    {
+        public string Language { get; set; }
+    }
+}
