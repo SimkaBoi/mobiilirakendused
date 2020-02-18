eec4e911e3fb2edf22095df056631d838705122c
SimkaBoi <simkaviiner@gmail.com>
2020-02-18 09:48:35 +0200

    add picture model and viewmodel

---
+++ MvvmTutorial/MvvmTutorial/Models/Picture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvmTutorial.Models
+{
+    public class Picture
+    {
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public byte Photo { get; set; }
+    }
+}
---
+++ MvvmTutorial/MvvmTutorial/ViewModels/PictureViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MvvmTutorial.ViewModels
+{
+    public class PictureViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public PictureViewModel()
+        {
+
+        }
+    }
+}
