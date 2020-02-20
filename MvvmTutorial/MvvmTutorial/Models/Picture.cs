3d28e989c71c063d7289608e1c621420ded035de
SimkaBoi <simkaviiner@gmail.com>
2020-02-20 09:30:56 +0200

    add pictures to listview

--- MvvmTutorial/MvvmTutorial/Models/Picture.cs
+++ MvvmTutorial/MvvmTutorial/Models/Picture.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FFImageLoading.Forms;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,6 +9,7 @@ namespace MvvmTutorial.Models
     {
         public string Title { get; set; }
         public string Desc { get; set; }
-        public byte Photo { get; set; }
+        public string Image { get; set; }
+        public DateTime Date { get; set; }
     }
 }
--- MvvmTutorial/MvvmTutorial/ViewModels/PictureViewModel.cs
+++ MvvmTutorial/MvvmTutorial/ViewModels/PictureViewModel.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using FFImageLoading.Forms;
+using MvvmTutorial.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Text;
 
 namespace MvvmTutorial.ViewModels
@@ -14,7 +17,27 @@ namespace MvvmTutorial.ViewModels
         }
         public PictureViewModel()
         {
+            Pictures = new List<Picture>
+            {
+                new Picture { Title = "title1", Desc = "desc1", Image = "http://loremflickr.com/600/600/nature?filename=simple.jpg", Date = DateTime.Now },
+                new Picture { Title = "title2", Desc = "desc2", Image = "http://loremflickr.com/600/600/nature?filename=simple.jpg", Date = DateTime.Now },
+                new Picture { Title = "title3", Desc = "desc3", Image = "http://loremflickr.com/600/600/nature?filename=simple.jpg", Date = DateTime.Now },
+                new Picture { Title = "title4", Desc = "desc4", Image = "http://loremflickr.com/600/600/nature?filename=simple.jpg", Date = DateTime.Now },
+            };
+        }
 
+        private List<Picture> _pictures;
+        public List<Picture> Pictures
+        {
+            get { return _pictures; }
+            set
+            {
+                if (_pictures != value)
+                {
+                    _pictures = value;
+                    OnPropertyChanged(nameof(Pictures));
+                }
+            }
         }
     }
 }
