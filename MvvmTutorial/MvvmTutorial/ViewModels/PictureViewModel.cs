cc7880383a829c5c5fcbdee0d5a68cc25f1f779d
SimkaBoi <simkaviiner@gmail.com>
2020-02-25 09:33:41 +0200

    add obamium

--- MvvmTutorial/MvvmTutorial/ViewModels/PictureViewModel.cs
+++ MvvmTutorial/MvvmTutorial/ViewModels/PictureViewModel.cs
@@ -23,6 +23,7 @@ namespace MvvmTutorial.ViewModels
                 new Picture { Title = "title2", Desc = "desc2", Image = "http://loremflickr.com/600/600/nature?filename=simple.jpg", Date = DateTime.Now },
                 new Picture { Title = "title3", Desc = "desc3", Image = "http://loremflickr.com/600/600/nature?filename=simple.jpg", Date = DateTime.Now },
                 new Picture { Title = "title4", Desc = "desc4", Image = "http://loremflickr.com/600/600/nature?filename=simple.jpg", Date = DateTime.Now },
+                new Picture { Title = "obamium", Desc = "oboomer", Image = "obamium.jpg", Date = DateTime.Now },
             };
         }
 
