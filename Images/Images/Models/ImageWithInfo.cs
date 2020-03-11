3bdebaef5da50ff264cd31edf71be6e3a5cd8858
SimkaBoi <simkaviiner@gmail.com>
2020-03-11 10:59:58 +0200

    add images project

---
+++ Images/Images/Models/ImageWithInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Images.Models
+{
+    public class ImageWithInfo
+    {
+        public string Name { get; set; }
+        public ImageSource Source { get; set; }
+    }
+}
---
+++ Images/Images/ViewModels/MainViewModel.cs
@@ -0,0 +1,112 @@
+﻿using Images.Models;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Images.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            Images = new ObservableCollection<ImageWithInfo>();
+            TakePictureCommand = new Command(OnTakePictureCommand);
+            PickPictureCommand = new Command(OnPickPictureCommand);
+        }
+
+        private ObservableCollection<ImageWithInfo> _images;
+        public ObservableCollection<ImageWithInfo> Images
+        {
+            get { return _images; }
+            set
+            {
+                if (_images != value)
+                {
+                    _images = value;
+                    OnPropertyChanged(nameof(Images));
+                }
+            }
+        }
+        public ICommand TakePictureCommand { get; private set; }
+
+        private async void OnTakePictureCommand()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await Application.Current.MainPage.DisplayAlert("Alert", "Camera can't be opened right now", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            var image = new ImageWithInfo();
+            image.Name = "New picture";
+            //image.Url = ImageSource.FromStream(file.Path);
+
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
+
+            Images.Add(image);
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ICommand PickPictureCommand { get; private set; }
+
+        private async void OnPickPictureCommand()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await Application.Current.MainPage.DisplayAlert("Alert", "Can't open album", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions
+            {
+                PhotoSize = PhotoSize.Full
+            });
+
+            if (file == null)
+                return;
+
+            var image = new ImageWithInfo();
+            image.Name = "New picture";
+            //image.Url = ImageSource.FromStream(file.Path);
+
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
+
+            Images.Add(image);
+        }
+    }
+}
