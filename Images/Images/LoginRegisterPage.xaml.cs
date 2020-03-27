57e4ac72e78cc4de19993d34f3fc4ce6e0b80e12
SimkaBoi <simkaviiner@gmail.com>
2020-03-27 16:00:50 +0200

    change login/register logic

--- Images/Images/LoginRegisterPage.xaml.cs
+++ Images/Images/LoginRegisterPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Images.ViewModels;
+﻿using Images.Data;
+using Images.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,66 @@ namespace Images
         public LoginRegisterPage()
         {
             InitializeComponent();
+        }
+
+        private async void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            var user = new UserData() { Username = Username.Text, Password = Password.Text };
+            List<UserData> users = await App.Database.GetUsersAsync();
+            if(user.Username == null || user.Password == null || user.Username == "" || user.Password == "")
+            {
+                OnError("Please fill all the fields!");
+            }
+            else
+            {
+                var name = users.Where(x => x.Username == user.Username).FirstOrDefault();
+                if (name == null)
+                {
+                    OnError("Wrong username or password!");
+                }
+                else if (user.Username == name.Username && user.Password == name.Password)
+                {
+                    var tabbedPage = new FirstTabbedPage();
+                    tabbedPage.BindingContext = user;
+                    await Navigation.PushAsync(tabbedPage);
+                }
+                else
+                {
+                    OnError("Wrong username or password!");
+                }
+            }
+        }
+
+        private async void OnRegisterButtonClicked(object sender, EventArgs e)
+        {
+            var user = new UserData() { Username = Username.Text, Password = Password.Text };
+            List<UserData> users = await App.Database.GetUsersAsync();
+            foreach (var name in users)
+            {
+                if(user.Username == "" || user.Username == null || user.Password == "" ||user.Password == null)
+                {
+                    OnError("Please fill all the fields!");
+                    break;
+                }
+                else if (name.Username == user.Username)
+                {
+                    OnError("Username is already taken!");
+                    break;
+                }
+                else
+                {
+                    await App.Database.SaveUserAsync(user);
+                    var tabbedPage = new FirstTabbedPage();
+                    tabbedPage.BindingContext = user;
+                    await Navigation.PushAsync(tabbedPage);
+                    break;
+                }
+            }
+        }
+
+        private async void OnError(string error)
+        {
+            Error.Text = error;
         }
     }
 }
